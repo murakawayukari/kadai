78a993e871e19cc770eaaa3f5fb446938de47f21
murakawayukari <yukari.m2000@gmail.com>
2020-10-14 05:32:56 +0000

    kadai-02

---
+++ kadai-02/index.js
@@ -0,0 +1,12 @@
+const firstName = 'yukari'
+const familyName = 'Murakawa'
+console.log(firstName + familyName)
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog = false
+const cat = false
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
+console.log(!(dog || cat))
