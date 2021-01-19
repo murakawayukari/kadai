434d451863712820a22f28f1bea8080f41c7a899
murakawayukari <yukari.m2000@gmail.com>
2021-01-19 14:34:57 +0000

    kadai-09

--- kadai-09/sample2.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-fs.writeFileSync('./kadai-09/hello.text','こんにちは')
-const file = fs.readFileSync('./kadai-09/hello.text')
-console.log(file.toString())
--- kadai-09/sample3.js
+++
@@ -1,3 +0,0 @@
-const cowsay = rquire('cowsay')
-const output = cowsay.say({text:'moo'})
-console.log(output)
